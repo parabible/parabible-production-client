9cc43a43c9daa12abc304470f111107eba47e149
jcuenod <j3frea+gitkraken@gmail.com>
2017-08-20 23:38:24 -0500

    Use fontsetting for rid units

--- src/components/RidView.jsx
+++ src/components/RidView.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import AccentUnit from './AccentUnit'
 import DataFlow from 'util/DataFlow'
 
+const fontSetting = () => {
+	return `${DataFlow.get("fontSetting")}, "SBL Biblit", "Open Sans", "Arial"`
+}
+
 const wlcDisplay = (rid, wlc, activeWid) => (
 	wlc.map((accentUnit, i) =>
 		<AccentUnit
-			style={{ fontFamily: DataFlow.get("fontSetting") }}
 			key={i}
 			verseNumber={i === 0 ? (rid % 1000) : false}
 			accentUnit={accentUnit}
@@ -26,7 +29,7 @@ const lxxDisplay = (rid, lxx, activeWid) => (
 const parallelView =({rid, activeWid, ridData}) => (
 	<div data-rid={rid} style={{display: "table", tableLayout: "fixed", width: "100%" }}>
 		{ridData.hasOwnProperty("wlc") ? (
-			<div style={{display: "table-cell", verticalAlign: "top", direction: "rtl", fontSize: "x-large", padding: "3px 5px"}}>
+			<div style={{ display: "table-cell", verticalAlign: "top", direction: "rtl", fontSize: "x-large", padding: "3px 5px", fontFamily: fontSetting()}}>
 				{wlcDisplay(rid, ridData.wlc, activeWid)}
 			</div>
 		) : ""}
@@ -49,7 +52,10 @@ const RidView = ({rid, ridData, activeWid}) => {
 	else {
 		switch(ridDataKeys[0]) {
 			case "wlc":
-				return <div style={{ display: "inline" }} data-rid={rid}>
+				return <div style={{
+						display: "inline",
+						fontFamily: fontSetting()
+					}} data-rid={rid}>
 					{wlcDisplay(rid, ridData.wlc, activeWid)}
 				</div>
 			default:
