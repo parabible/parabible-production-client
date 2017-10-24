b9f780c9be8dcb06794ffa58ec5aa5d3fce151d3
jcuenod <j3frea+gitkraken@gmail.com>
2017-10-24 14:45:00 -0500

    Update SW cache on request but serve from cache (trailing updates)

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-pwa-cache-v1'
+var CACHE = 'cache-and-update'
 var urlsToCache = [
 	'/bundle.js',
 	'/fonts/sblbiblit.css',
@@ -8,27 +8,41 @@ var urlsToCache = [
 	'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
 	'https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/5.0.1/css/fabric.min.css'
 ]
+
+// On install, cache some resources.
 self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(cache => {
-				// Open a cache and cache our files
-				return cache.addAll(urlsToCache)
-			})
-	)
+	console.log('The service worker is being installed.')
+	event.waitUntil(() => {
+		return caches.open(CACHE).then(cache => {
+			return cache.addAll(urlsToCache)
+		})
+	})
 })
 
 self.addEventListener('fetch', event => {
-	// console.log(event.request.url)
-	// event.request.json().then(x => {
-	// 	console.log(x)
-	// })
-	event.respondWith(
-		caches.match(event.request).then(response => {
-			// if (response) {
-			// 	console.log("From cache!", event.request)
-			// }
-			return response || fetch(event.request)
-		})
-	)
+	event.respondWith(fromCache(event.request))
+	event.waitUntil(update(event.request))
 })
+
+function fromCache(request) {
+	return caches.open(CACHE).then(cache => {
+		return cache.match(request).then(matching => {
+			if (matching) {
+				return matching
+			}
+			else {
+				return fetch(request).then(response => {
+					return response
+				})
+			}
+		})
+	})
+}
+
+function update(request) {
+	return caches.open(CACHE).then(cache => {
+		return fetch(request).then(response => {
+			return cache.put(request, response)
+		})
+	})
+}
