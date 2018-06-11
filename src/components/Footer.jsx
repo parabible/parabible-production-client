bd328519521beccdd2ae7a5d6a23044e60bc077d
James Cuénod <j3frea+gitkraken@gmail.com>
2018-06-11 14:46:28 -0500

    Added donorbox support link

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Footer = () => {
+	return <div style={{
+		fontFamily: "Open Sans, sans",
+		fontSize: "10px",
+		display: "flex",
+		backgroundColor: "#d0d0d0"
+	}}>
+		<div style={{
+			flex: 1,
+			textAlign: "right",
+			padding: "15px"
+		}}>
+			<a href="https://parabible.com">https://parabible.com</a> was built by James Cuénod.<br />
+			All the code is available on <a href="https://github.com/parabible">https://github.com/parabible</a>
+		</div>
+		<div style={{
+			flex: 1,
+			padding: "15px",
+			display: "flex",
+			alignItems: "center"
+		}}>
+			<a className="dbox-donation-button"
+				href="https://donorbox.org/support-parabible"
+				style={{
+					display: "flex", 
+					alignItems: "center", 
+					backgroundColor: "#2d81c5", 
+					borderRadius: "2px", 
+					padding: "5px 10px", fontSize: "12px", color: "#fff", textDecoration: "none"
+				}
+			}>
+				<div style={{ paddingRight: "5px", fontWeight: "bold" }}>
+					SUPPORT PARABIBLE
+				</div>
+				<div>
+					<img src="https://d1iczxrky3cnb2.cloudfront.net/red_logo.png" />
+				</div>
+			</a>
+		</div>
+	</div>
+
+}
+export default Footer
