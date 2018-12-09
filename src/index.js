9b26a7ff49c5e283033a49fa46ac92df84fb8a01
James Cuénod <j3frea+gitkraken@gmail.com>
2018-12-08 18:35:34 -0600

    Added strip diacritic option (cf. parabible/parabible-client#11)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import App from 'components/App'
 
 import HistoryManager from 'util/HistoryManager'
+import ClipboardHelper from 'util/ClipboardHelper'
 
 render(
 	<App />,
