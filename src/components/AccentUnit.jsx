32b4ce8eabce2ff656869e7112640e5630aba1f9
James Cuénod <j3frea+gitkraken@gmail.com>
2019-05-28 22:28:17 -0500

    Use fewer wrapping spans (and auto-indent)

--- src/components/AccentUnit.jsx
+++ src/components/AccentUnit.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import WordBit from './WordBit'
 
-const AccentUnit = ({verseNumber, accentUnit, activeWid}) => (
+const AccentUnit = ({ verseNumber, accentUnit, activeWid }) => (
 	<span className="accentUnit">
 		{verseNumber !== false ? (
 			<span style={{
-					color: "#ea4300",
-					verticalAlign: "top",
-					fontSize: "35%",
-					fontWeight: "bold"
-				}}>
+				color: "#ea4300",
+				verticalAlign: "top",
+				fontSize: "35%",
+				fontWeight: "bold"
+			}}>
 				{verseNumber}&nbsp;
 			</span>) : null}
 		{accentUnit.map((bit, i) => (
-			<WordBit key={i} wbit={bit} activeWid={activeWid} />
+			<WordBit key={i} keyIndex={i} wbit={bit} activeWid={activeWid} />
 		))}
 	</span>
 )
