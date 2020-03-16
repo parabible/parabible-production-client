f9f88c45ae27577e17d3090652ff61d24d09f9fa
James Cuénod <j3frea+gitkraken@gmail.com>
2020-03-16 12:46:43 -0500

    Improved accent stripping function to remove Paseq (and qamets qatan but I think those are normalised out)

--- src/util/ClipboardHelper.js
+++ src/util/ClipboardHelper.js
@@ -3,8 +3,13 @@ import DataFlow from './DataFlow'
 document.addEventListener('copy', (e) => {
     if (!DataFlow.get("stripDiacritics")) return
 
-    const selectedText = window.getSelection().toString()
-    const strippedText = selectedText.replace(/[\u0590-\u05AF\u05BD]/g,"")
+	const selectedText = window.getSelection().toString()
+	// 0590-05AF = All accents (except these the ones in the C range
+	//                         [but the C range includes stuff like sof pasuq and sin/shin dots])
+	// 05BD      = Meteg
+	// 05C0      = PASEQ {legarmeh}
+	// 05C7      = Qamats Qatan
+    const strippedText = selectedText.replace(/[\u0590-\u05AF\u05BD\u05C0\u05C7]/g,"")
 
     e.clipboardData.setData('text/plain', strippedText)
 
