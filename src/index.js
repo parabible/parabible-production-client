8652e36a113ee7df56a6bbd0780f4b117fb804fd
James Cuénod <j3frea+gitkraken@gmail.com>
2020-04-29 11:40:14 -0500

    Moving goatcounter faker to actually help things

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from 'components/App'
 
-import HistoryManager from 'util/HistoryManager'
-HistoryManager.init()
-import ClipboardHelper from 'util/ClipboardHelper'
-ClipboardHelper.init()
-
+// Make a fake a goatcounter object to give time to load it so HistoryManager doesn't crash things all the time
 if (!("goatcounter" in window)) {
 	window.goatcounter = {
 		count: () => { console.error("Failed to hit goatcoutner. GC loaded too late (I think).") }
 	}
 }
+
+import HistoryManager from 'util/HistoryManager'
+HistoryManager.init()
+import ClipboardHelper from 'util/ClipboardHelper'
+ClipboardHelper.init()
 
 render(
 	<App />,
