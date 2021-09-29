e3c359ae770a84b90ab9573e14ba81c74ea34f7e
James Cuénod <jcuenod@users.noreply.github.com>
2021-09-29 16:13:42 -0500

    Use React.memo

--- src/components/AccentUnit.jsx
+++ src/components/AccentUnit.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import WordBit from './WordBit'
 
-const AccentUnit = ({ verseNumber, accentUnit, activeWid }) => [
-	verseNumber !== false ? (
-		<span key={"v" + verseNumber} style={{
+const PossibleVerseNumber = ({ verseNumber }) =>
+	verseNumber === false
+		? null
+		: <span key={"v" + verseNumber} style={{
 			color: "#ea4300",
 			verticalAlign: "top",
 			fontSize: "35%",
 			fontWeight: "bold"
 		}}>
 			{verseNumber}&nbsp;
-			</span>) : null,
-	...accentUnit.map((bit, i) => (
-		<WordBit key={i} keyIndex={i} wbit={bit} activeWid={activeWid} />
-	))
-]
+		</span>
+
+const AccentUnit = React.memo(({ verseNumber, accentUnit, activeWid }) =>
+	<>
+		<PossibleVerseNumber verseNumber={verseNumber} />
+		{accentUnit.map((bit, i) => (
+			<WordBit key={i} keyIndex={i} wbit={bit} activeWid={activeWid} />
+		))}
+	</>
+)
 
 export default AccentUnit
